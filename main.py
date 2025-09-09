e93489050d5a2c6b26ac3913f3e6aeae9cf9f17d
Anita Gajewska <gajewskaanita0@gmail.com>
2025-09-09 21:51:28 +0200

    Solution

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 import json
 
+
 def main() -> None:
-    with open('players.json', 'r') as f:
+    with open("players.json", "r") as f:
         players_dict = json.load(f)
 
-
     for nickname, player_data in players_dict.items():
-        race_data = player_data['race']
+        race_data = player_data["race"]
         race = Race.objects.get_or_create(
-            name=race_data['name'],
-            defaults={"description": race_data.get('description', "")}
+            name=race_data["name"],
+            defaults={"description": race_data.get("description", "")}
         )[0]
 
-        for skill_data in race_data.get('skills', []):
+        for skill_data in race_data.get("skills", []):
             Skill.objects.get_or_create(
-                name=skill_data['name'],
-                defaults={"bonus": skill_data.get('bonus', ""), "race": race}
+                name=skill_data["name"],
+                defaults={"bonus": skill_data.get("bonus", ""), "race": race}
             )
 
-        guild_data = player_data['guild']
+        guild_data = player_data["guild"]
         if guild_data:
             guild = Guild.objects.get_or_create(
-                name=guild_data['name'],
-                defaults={"description": guild_data.get('description', "")}
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description", "")}
             )[0]
         else:
             guild = None
@@ -33,15 +33,12 @@ def main() -> None:
         Player.objects.get_or_create(
             nickname= nickname,
             defaults={
-                "email": player_data.get('email', ""),
-                "bio": player_data.get('bio', ""),
+                "email": player_data.get("email", ""),
+                "bio": player_data.get("bio", ""),
                 "race": race,
                 "guild": guild
             }
         )
-
-    pass
-
 
 if __name__ == "__main__":
     main()
